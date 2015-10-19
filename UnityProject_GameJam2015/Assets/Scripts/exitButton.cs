223fe03bf8599bec79ac45f0cdb91276b4a765fc
MarcKami <Marc.kami1991@gmail.com>
2015-10-19 19:12:51 +0200

    Añadido menu con boton start y musica

---
+++ UnityProject_GameJam2015/Assets/Scripts/exitButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exitButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // When click on Exit button you quit the game
+    void OnMouseDown()
+    {
+        Application.Quit();
+    }
+}
---
+++ UnityProject_GameJam2015/Assets/Scripts/startButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // When you click START the game starts
+    void OnMouseDown()  {
+        Application.LoadLevel("Gameplay");
+    }
+}
