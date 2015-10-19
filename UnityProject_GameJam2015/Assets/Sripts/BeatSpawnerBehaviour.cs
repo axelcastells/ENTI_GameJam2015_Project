6bd62cb4de2991a67c9ac1ea6123d1214b3c574e
AkuseruSan <AkuseruAkai@gmail.com>
2015-10-19 13:59:20 +0200

    BeatRate System (on Develop)

---
+++ UnityProject_GameJam2015/Assets/Sripts/BeatSpawnerBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatSpawnerBehaviour : MonoBehaviour {
+
+    private GameObject lastSpawned;
+	// Use this for initialization
+	void Start () {
+	    
+        
+	}
+	
+    void FixedUpdate()
+    {
+        if (BeatSystem.SetTheBeat() == true)
+        {
+            SpawnBeat();
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Translate(new Vector3(BeatSystem.beatRateMaster * Time.deltaTime, 0, 0));
+	}
+
+    private void SpawnBeat()
+    {
+        Debug.Log("BeatSpawn!");
+        lastSpawned = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        lastSpawned.GetComponent<Renderer>().material.color = Color.blue;
+        lastSpawned.transform.position = this.transform.position;
+
+        lastSpawned.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+        //lastSpawned.transform.parent = this.transform;
+    }
+}
---
+++ UnityProject_GameJam2015/Assets/Sripts/BeatSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class BeatSystem {
+
+    public static float beatRateMaster = 1f;
+    public static float beatRateCurrent = 0;
+
+    public static bool SetTheBeat()
+    {
+        if (beatRateCurrent >= beatRateMaster)
+        {
+            beatRateCurrent = 0;
+            return true;
+        }
+
+        beatRateCurrent += (1 * Time.deltaTime);
+
+        return false;
+    }
+}
