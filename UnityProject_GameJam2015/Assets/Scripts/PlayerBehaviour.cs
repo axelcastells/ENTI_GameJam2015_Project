cac343a3705bc84e3587ecf891f542df52287295
AkuseruSan <AkuseruAkai@gmail.com>
2015-10-20 21:26:12 +0200

    Optimized Button Functions

--- UnityProject_GameJam2015/Assets/Scripts/PlayerBehaviour.cs
+++ UnityProject_GameJam2015/Assets/Scripts/PlayerBehaviour.cs
@@ -3,14 +3,32 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
+    private enum PlayerState { ALIVE, DEAD }
+    private PlayerState playerState;
+
 	// Use this for initialization
 	void Start () {
-	
+        playerState = PlayerState.ALIVE;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
-        //transform.Translate((float)BeatSystem.bps * Time.fixedDeltaTime, 0, 0);
-	}
+        switch (playerState)
+        {
+            case PlayerState.ALIVE:
+                {
+                    this.transform.Translate((float)BeatSystem.bps * Time.deltaTime, 0, 0);
+                }
+                break;
+            case PlayerState.DEAD:
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
+         //transform.Translate((float)BeatSystem.bps * Time.fixedDeltaTime, 0, 0);
+    }
 }
